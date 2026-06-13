f14d574ead462b57037d4953ef6ffc221ee36d68
mutkukucuk <muhammetutku@gmail.com>
2026-06-13 13:43:17 +0300

    Initial commit

---
+++ src/main/java/com/utkukucuk/commerceplatform/CommercePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.utkukucuk.commerceplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommercePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommercePlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/utkukucuk/commerceplatform/CommercePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utkukucuk.commerceplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommercePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
