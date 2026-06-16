5553b05d2d8d22dc3dd904dd0869e43f3c5ce72b
mutkukucuk <muhammetutku@gmail.com>
2026-06-16 14:14:06 +0300

    feat: add address entity

--- src/main/java/entity/Address.java
+++ src/main/java/entity/Address.java
@@ -1,4 +1,41 @@
 package entity;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "address")
 public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String street;
+    private String city;
+    private String state;
+    private String country;
+    private String zipcode;
+
+
+    @OneToOne
+    @JoinColumn(name = "address_id")
+    private Address address;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+
+
 }
